a333b81db3e2e225bd1db8e2645a99811c390b37
Mateus Assis <41522926+mateusassis02@users.noreply.github.com>
2023-01-05 17:37:56 -0300

    notificacao

--- notificacaoFalha.sh
+++ notificacaoFalha.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H 'Content-Type: application/json' -d '{"text": "A Pipeline falhou."}' https://anvisabr.webhook.office.com/webhookb2/478e3a2f-b6ae-4ed1-b575-3b0bff820f52@b67af23f-c3f3-4d35-80c7-b7085f5edd81/IncomingWebhook/78e8edbc5c64492da814caafbee3c301/6952b15b-2fb2-4617-8cdc-bc64a1a5333e
+curl -X POST "https://api.telegram.org/bot5639787607:AAE4n7eJC_ohqRc3KAvWtLjDofurHEqy9Os/sendMessage" -d "chat_id=-672103735&text=Pipeline executada com falhas!"
